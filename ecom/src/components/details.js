f1dc34b63a4636f51b02406f52176887137f43a5
InAudibleDayne <daynebechtold@hotmail.com>
2019-11-26 16:36:23 -0700

    finished sign up form and grid and prepped reroute to account component

--- ecom/src/components/details.js
+++ ecom/src/components/details.js
@@ -1,16 +1,30 @@
 import React, { Component } from 'react';
 
 class Details extends Component {
+    
+    renderChildren = () => {
+        const { links, infos } = this.props;
+        let children = [];
+        if(links) {
+            children = links.map(link => {
+                return <a key={link.__id} onClick={link.onClick} className='details__link'>{link.title}</a>
+            })
+        } else if(infos) {
+            children = infos.map(info => {
+                return <div key={info.__id} className='details__link details__item'>{info.title}</div>
+            })
+        }
+        return children;
+    }
+
   render() {
-      const { className, title, links } = this.props;
+      const { className, title, links, infos } = this.props;
     return (
         <div className={`${className} details`}>
             <div className='details__title'>{title}</div>
             <div className='details__links'>
                 {
-                    links.map(link => {
-                        return <a key={link._id} onClick={link.onClick} className='details__link'>{link.title}</a>
-                    })
+                    this.renderChildren()
                 }
             </div>
         </div>
