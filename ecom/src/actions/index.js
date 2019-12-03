dd884e943677dbf3d7ecd081fc12c6e2217c9454
InAudibleDayne <daynebechtold@hotmail.com>
2019-12-03 14:21:09 -0700

    implemented set purchase detail action creator and reducer

--- ecom/src/actions/index.js
+++ ecom/src/actions/index.js
@@ -5,12 +5,14 @@ import {
 } from './headerNavbar';
 
 import {
-    fetchUserPurchases
+    fetchUserPurchases,
+    setPurchaseDetail
 } from './user';
 
 export {
     setHeaderLinks,
     setNavbarLinks,
     changeNavbarActive,
-    fetchUserPurchases
+    fetchUserPurchases,
+    setPurchaseDetail
 }
