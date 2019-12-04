093c4ebcaf2deef1db33b1e50e2c95fa2e274e6b
InAudibleDayne <daynebechtold@hotmail.com>
2019-12-04 15:37:47 -0700

    developed shopreducer and fetch shop categories action creator

---
+++ ecom/src/components/shop/shop.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+
+import { connect } from 'react-redux';
+import * as actions from '../../actions';
+
+class Shop extends Component {
+    componentDidMount() {
+
+        const headerLinks = [
+            {
+                _id: 1,
+                title: 'Login',
+                path: '/signin'
+            }
+        ]
+        this.props.setHeaderLinks(headerLinks);
+        this.props.fetchShopCategories();
+        //fetch shop products action creator
+        //fetch navbar links
+            //set navbar links
+            //filter products with links
+    }
+  render() {
+    return (
+        <div className='shop'>
+            Shop
+            {/*
+            shop searchbar component
+            shop product
+            cart button
+            */}
+        </div>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+    return { state }
+}
+
+Shop=connect(mapStateToProps, actions)(Shop);
+
+export default Shop;
