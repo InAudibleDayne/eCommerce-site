6c69ecbdf2e746a3fcf49c128b04a731169be6fe
InAudibleDayne <daynebechtold@hotmail.com>
2019-12-12 16:04:53 -0700

    added a fetch action to get cart products

--- ecom/src/actions/user.js
+++ ecom/src/actions/user.js
@@ -1,6 +1,7 @@
 import {
     SET_USER_PURCHASES,
-    SET_PURCHASE_DETAIL
+    SET_PURCHASE_DETAIL,
+    SET_CART_PRODUCTS
 } from './types';
 
 export function setPurchaseDetail(_id){
@@ -99,6 +100,36 @@ export function fetchUserPurchases() {
                     shippingAddress: '1234 West State st'
                 }
             }
+        ]
+    })
+}
+
+export function fetchCartProducts() {
+    return ({
+        type: SET_CART_PRODUCTS,
+        payload: [
+            {
+                _id: 0,
+                product: {
+                    _id: 0,
+                    title: 'JavaScript in the Browser',
+                    description: 'lorem ipsum',
+                    price: 1.99,
+                    belongsTo: [0, 1]
+                },
+                quantity: 2
+            },
+            {
+                _id: 1,
+                Product: {
+                    _id: 1,
+                    title: 'Graph Database',
+                    description: 'lorem ipsum',
+                    price: 1.99,
+                    belongsTo: [0, 5]
+                },
+                quantity: 1
+            },
         ]
     })
 }
